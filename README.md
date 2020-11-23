bf68ebe69dc4a092789c2af09415e4a9794a2ca5
abhisekhaldar <abhisekhaldar@yahoo.co.in>
2020-11-23 10:28:25 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # test-package789
+Test Readme
---
+++ index.js
@@ -0,0 +1,7 @@
+function isNullOrEmpty(input){
+
+	return (input == undefined || input == '' || input == null);
+
+}
+
+modules.exports = isNullOrEmpty;
